819dd4e793e58c98fc3133c309464c8691efb796
Ummasalma Jamil <ummasalmaj2004@gmail.com>
2026-09-04 18:39:17 +0800

    Add ButtonDemo.kt to simulate button click action

---
+++ Unit2_Codelabs/codes/ButtonDemo.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Simulating a button click with a function
+    fun onButtonClick() {
+        println("Button clicked! Action performed.")
+    }
+
+    // Pretend user clicks the button
+    println("User taps the button...")
+    onButtonClick()
+}
