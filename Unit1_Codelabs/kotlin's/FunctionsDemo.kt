7016e8e428ab1da89fb80b3ff9fa309b767e85f4
Ummasalma Jamil <ummasalmaj2004@gmail.com>
2026-09-04 18:15:45 +0800

    Add greetUser function to greet users

---
+++ Unit1_Codelabs/kotlin's/FunctionsDemo.kt
@@ -0,0 +1,7 @@
+fun greetUser(name: String): String {
+    return "Welcome, $name, to Kotlin!"
+}
+
+fun main() {
+    println(greetUser("Ummasalma"))
+}
--- Unit1_Codelabs/kotlin's/VariablesDemo.kt
+++
@@ -1,5 +0,0 @@
-fun main() {
-    val name = "Ummasalma"
-    val age = 21
-    println("My name is $name and I am $age years old.")
-}
